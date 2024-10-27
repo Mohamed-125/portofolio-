05b3fd6d27f9c12548914fb7ac26fd9ada66286b
Mohamed-125 <mohamedibrahim@gmail.com>
2024-10-28 02:49:15 +0300

    adding services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
     {
       title: "Web Development",
       description:
-        "I will help you to create your professional landing page,ecommerce,portofolio ",
+        "I will help you to create your professional landing page, ecommerce, portofolio ",
     },
     {
       title: "Web Design",
@@ -17,13 +17,15 @@ const Services = () => {
 
   return (
     <div className="container text-text py-20">
-      <h2 className="text-3xl font-bold mb-6">Services</h2>
+      <h2 className="title">Services</h2>
       <div className="flex flex-wrap  gap-4">
         {Services.map((service, index) => (
           <div className="border-text cursor-pointer title-stroke group min-w-[270px] flex-1 border rounded-lg py-5 px-4">
-            <h3 className=" text-4xl mb-2 text-[transparent]">0{index + 1}</h3>
-            <h4 className="text-xl mb-3  font-bold">{service.title}</h4>
-            <p className="text-sm">{service.description}</p>
+            <h3 className=" text-5xl font-bold mb-2 text-[transparent]">
+              0{index + 1}
+            </h3>
+            <h4 className="text-2xl mb-3  font-bold">{service.title}</h4>
+            <p className="text-[17.5]px">{service.description}</p>
           </div>
         ))}
       </div>
