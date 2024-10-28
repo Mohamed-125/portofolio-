38d9661e4f2ff4173e62ee1a67238828357296ce
Mohamed-125 <mohamedibrahim@gmail.com>
2024-10-28 23:33:19 +0300

    adding new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,6 +14,15 @@ const Projects = () => {
       image: "https://i.im.ge/2023/03/21/Df5JGY.generated-file-name.png",
     },
     {
+      title: "Skywings tours website",
+      github:
+        "https://github.com/Mohamed-125/Skywings-tours-website",
+
+      image:
+        "https://i.im.ge/2024/10/28/kRD1t4.Screenshot-2024-10-28-175845.png",
+      url: "https://mohamed-125.github.io/Skywings-tours-website/",
+    },
+    {
       title: "NFT Collection Website",
       github:
         "https://github.com/Mohamed-125/NFT-Collection-Website-Landing-page-with-React-JS",
@@ -56,7 +65,7 @@ const Projects = () => {
   ];
 
   return (
-    <div className="container text-text py-20" id="projects">
+    <div className="container py-20 text-text" id="projects">
       <Reveal>
         <h1 className="title">
           My Projects<span className="text-primary">.</span>
@@ -65,11 +74,11 @@ const Projects = () => {
       <motion.div
         initial={{ translateY: "80px", opacity: 0 }}
         animate={{ translateY: "0px", opacity: 1 }}
-        className="grid gap-3 grid-cols-1 md:grid-cols-2  justify-center"
+        className="grid justify-center grid-cols-1 gap-3 md:grid-cols-2"
         style={{ gridTemplateRows: "repeat(2 , minmax(100px, auto))" }}
       >
         {projects.map((project) => (
-          <div className="w-full grid gap-6 grid-rows-subgrid row-span-2 py-5 px-4 bg-background-very-light rounded-lg">
+          <div className="grid w-full row-span-2 gap-6 px-4 py-5 rounded-lg grid-rows-subgrid bg-background-very-light">
             <div className="">
               <img
                 src={project.image}
@@ -77,17 +86,17 @@ const Projects = () => {
               />
             </div>
             <div>
-              <div className="justify-between flex items-center gap-3">
+              <div className="flex items-center justify-between gap-3">
                 <Reveal>
                   <h5 className="font-bold text-[19px]">{project.title}</h5>
                 </Reveal>
                 <Reveal>
                   <div className="flex gap-2">
                     <a href={project.github} target="_blank">
-                      <FaGithub className="text-2xl text-primary  opacity-60 duration-200 transition-all hover:opacity-100 cursor-pointer " />
+                      <FaGithub className="text-2xl transition-all duration-200 cursor-pointer text-primary opacity-60 hover:opacity-100 " />
                     </a>
                     <a href={project.url} target="_blank">
-                      <IoMdExit className="text-2xl  text-primary opacity-60 duration-200 transition-all hover:opacity-100 cursor-pointer " />
+                      <IoMdExit className="text-2xl transition-all duration-200 cursor-pointer text-primary opacity-60 hover:opacity-100 " />
                     </a>
                   </div>
                 </Reveal>
